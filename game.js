37ab60fdc0717ef9911266fd1ea531466746c739
Jean Phillippe Bethencourt <hello@jeanphillippe.com>
2025-07-24 20:25:17 -0300

    Update game.jsc

--- game.js
+++ game.js
@@ -1,3 +1,4 @@
+
         const USE_SPRITE_PLAYER = true; // Set to false for cylinder player
         const USE_SPRITE_TERRAIN = true; // Set to true for sprite terrain, false for 3D tiles
         class GameEngine {
